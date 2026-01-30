bd76d173be67affe288a1c94a5e296a217810fc2
furusato33 <furusato.shota.fu3@naist.ac.jp>
2026-01-30 12:53:21 +0900

    add my name to headher

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -51,6 +51,10 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
+            <h1 className="text-center mt-4 mb-4">
+                ToDo App by Shota Furusato
+            </h1>
+
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
@@ -64,6 +68,8 @@ function TodoListCard() {
                 />
             ))}
         </React.Fragment>
+
+
     );
 }
 
